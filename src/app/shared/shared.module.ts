31f4c9e31eb1481d5777d9ec602d66ee005a794d
WiiruxCruz <hialf.92@gmail.com>
2021-11-25 13:19:00 -0600

    Refactorización de componente sidebar y routerLink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent
